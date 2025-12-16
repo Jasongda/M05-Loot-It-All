8cc15c7a794910d3ad23213ff049375f9477a3ce
Jasongda <jayu16@s.sfusd.edu>
2025-12-16 12:37:22 -0800

    M5L10+Final Project M5 Sounds and SFX Completed

--- lessons/item.gd
+++ lessons/item.gd
@@ -1,4 +1,5 @@
-extends Node2D
+extends Area2D
+
 @onready var sfx_player: AudioStreamPlayer = _find_sfx_player()
 
 func _find_sfx_player() -> AudioStreamPlayer:
@@ -6,15 +7,15 @@ func _find_sfx_player() -> AudioStreamPlayer:
 		if child is AudioStreamPlayer:
 			return child
 	return null
-	
-func _on_area_entered(area_that_entered: Area2D) -> void:
-	get_node("CollisionShape2D").set_deferreed("disabled", true)
+
+func _on_area_entered(_area_that_entered: Area2D) -> void:
+	get_node("CollisionShape2D").set_deferred('disabled', true)
 	visible = false
-	
+
 	if sfx_player:
 		sfx_player.play()
-		await sfx_player.finished
-	
+	await sfx_player.finished
+
 	queue_free()
 
 func _ready() -> void:
